dbc002eb7ede1a8ec8d73c29ff0fb48d6c7863ed
Tim Schoof <t.schoof@psychlangsci.ucl.ac.uk>
2018-09-18 12:08:49 +0100

    update Stuart

--- PsychoacousticTrainingIADs.m
+++ PsychoacousticTrainingIADs.m
@@ -78,8 +78,8 @@ handles.numExamples=str2double(get(handles.nExamples,'String'));
 handles.numTests=str2double(get(handles.nTests,'String'));
 
 %% OBS! Hard coding of any extra args that need to be passed
-handles.ExtraArgsArray = {'usePlayrec', 0, 'RMEslider', 'FALSE','preSilence', 00};
-% handles.ExtraArgsArray = {'usePlayrec', 1, 'RMEslider', 'TRUE','preSilence', 0};
+% handles.ExtraArgsArray = {'usePlayrec', 0, 'RMEslider', 'FALSE','preSilence', 00};
+handles.ExtraArgsArray = {'usePlayrec', 1, 'RMEslider', 'TRUE','preSilence', 0};
 
 
 % Update handles structure
